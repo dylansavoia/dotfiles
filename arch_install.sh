ac99988fbef59f3ee43aadec1c510fb05b45d86e
dylansavoia <dylansavoia@gmail.com>
2019-10-14 17:55:28 +0200

    Update

--- arch_install.sh
+++ arch_install.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Check UEFI Support
+echo "Verifying boot type (BIOS, UEFI)..."
+if [ -e /sys/firmware/efi/efivars ]; then
+    echo "UEFI Supported"
+    uefi=true
+else
+    echo "UEFI Not Supported, Proceed (y|N)?"
+    read answ
+    [[ $answ != "y" ]] && exit 
+    uefi=false
+fi
+
+echo "You're going to check to connect to Wi-Fi. If not needed just Ctrl-C:"
+read
+wifi-menu
+
 # Mount Partitions
-echo "The installer assumes partitioning already performed. Continue? (Y|n)"
+echo "The installer assumes partitions already created (not formatted). Continue? (Y|n)"
 read answ
 [[ $answ == "n" ]] && exit
 
-echo -e "Specify the names for the root, home, boot and swap devices\n(es. sda1 sda2 sda3):"
+echo -e "Specify the names for the root, home, boot and swap devices\n(es. sda1 sda2 sda3 sda4):"
 read partitions
 partitions=( $partitions )
 
@@ -22,23 +38,11 @@ mount "/dev/${partitions[0]}" /mnt
 
 mkdir -p /mnt/home
 echo "/dev/${partitions[1]} --> /mnt/home"
-mount "/dev/${partitions[1]}" /mnt
+mount "/dev/${partitions[1]}" /mnt/home
 
 mkdir -p /mnt/boot
 echo "/dev/${partitions[2]} --> /mnt/boot"
 mount "/dev/${partitions[2]}" /mnt/boot
-
-# Check UEFI Support
-echo "Verifying boot type (BIOS, UEFI)..."
-if [ -e /sys/firmware/efi/efivars ]; then
-    echo "UEFI Supported"
-    uefi=true
-else
-    echo "UEFI Not Supported, Proceed (y|N)?"
-    read answ
-    [[ $answ != "y" ]] && exit 
-    uefi=false
-fi
 
 # Install Arch Linux
 timedatectl set-ntp true
@@ -47,45 +51,3 @@ pacstrap /mnt base base-devel
 # Setup
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-
-## Locale
-echo -e "Set Region and City as in /usr/share/zoneinfo\n(es. Europe Rome):"
-read city
-city=( $city )
-ln -sf "/usr/share/zoneinfo/${city[0]}/${city[1]}" /etc/localtime
-
-hwclock --systohc
-
-echo -e "Set Locale as in /etc/locale.gen (es. en_US):"
-read locale
-sed -e "/$locale/s/^#//"
-locale-gen
-
-flocale=`grep $locale | head -1`
-echo "LANG=$flocale" > /etc/locale.conf
-
-echo "Choose name for computer:"
-read hostname
-echo $hostname > /etc/hostname
-
-echo -e "127.0.0.1\tlocalhost\n ::1\tlocalhost\n 127.0.1.1\t$hostname.localdomain\t$hostname" > /etc/hosts
-
-echo "Set root password:"
-passwd
-
-# Install GRUB
-pacman -S grub
-
-if [[ $uefi = true ]]; then
-    echo "Install UEFI GRUB"
-    pacman -S efibootmgr
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-else
-    echo "Install BIOS GRUB"
-    grub-install --target=i386-pc "/dev/${partitions[2]}"
-fi
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Done
-echo "You can now Reboot"
--- arch_setup.sh
+++ arch_setup.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 
+echo "This is to be executed from the new system. Do you know what you're doing?"
+read
+
+# Check UEFI Support
+echo "Verifying boot type (BIOS, UEFI)..."
+if [ -e /sys/firmware/efi/efivars ]; then
+    echo "UEFI Supported"
+    uefi=true
+else
+    echo "UEFI Not Supported, Proceed (y|N)?"
+    read answ
+    [[ $answ != "y" ]] && exit 
+    uefi=false
+fi
+
+# Locale
+echo -e "Set Region and City as in /usr/share/zoneinfo\n(es. Europe Rome):"
+read city
+city=( $city )
+ln -sf "/usr/share/zoneinfo/${city[0]}/${city[1]}" /etc/localtime
+
+hwclock --systohc
+
+echo -e "Set Locale as in /etc/locale.gen (es. en_US):"
+read locale
+sed -e "/$locale/s/^#//" /etc/locale.gen
+locale-gen
+
+flocale=`grep $locale /etc/locale.gen | head -1`
+echo "LANG=$flocale" > /etc/locale.conf
+
+echo "Choose name for computer:"
+read hostname
+echo $hostname > /etc/hostname
+
+echo -e "127.0.0.1\tlocalhost\n ::1\tlocalhost\n 127.0.1.1\t$hostname.localdomain\t$hostname" > /etc/hosts
+
+echo "Set root password:"
+passwd
+
+# Install GRUB
+pacman -S grub
+
+if [[ $uefi = true ]]; then
+    echo "Install UEFI GRUB"
+    pacman -S efibootmgr
+    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+else
+    echo "Install BIOS GRUB"
+    grub-install --target=i386-pc "/dev/${partitions[2]}"
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Done
+echo "You can now Reboot"
+
 # Create User  
 echo -e "Create default user and edit /etc/sudoers file\n(You may just want to uncomment the wheel option)\nUsername: "
 read username
@@ -58,6 +115,7 @@ echo "Creating RSA key for server..."
 ssh-keygen -t rsa
 ssh-copy-id dylansavoia@dylansavoia.sytes.net
 
+echo "Check arch_setup.sh comments for last steps."
 # Setup Imagemagick for pdf preview
 # ... change in /etc/ImageMagick-6/policy.xml: pdf none to read|write
 # Manually? pdfjs (for qutebrowser) tesseract-ocr ardour conda lightdm
